054c418af038b1b75b7baa2dd1f2144f75cc4b4f
maher9393267111 <89878167+maher9393267111@users.noreply.github.com>
2023-05-25 20:36:00 +0300

    add

--- pages/_app.js
+++ pages/_app.js
@@ -14,6 +14,7 @@ import { Provider } from "react-redux";
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
 
+
 // translate
 import { appWithTranslation } from "next-i18next";
 
